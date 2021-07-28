9d4fd6e1a0afe1756686d66bbd2b6f307fbff322
TwOnEnd <58563421+TwOnEnd@users.noreply.github.com>
2021-07-28 21:38:39 +0800

    Add files via upload

---
+++ ExplodeLog/BDS插件/Leve.hpp
@@ -0,0 +1,10 @@
+
+#define FETCH(type, ptr) (*reinterpret_cast<type*>(ptr))
+
+struct Level 
+{
+	std::vector<Actor*> getAllPlayers()
+	{
+		return FETCH(std::vector<Actor*>, this + 112);//IDA Level::forEachPlayer
+	}
+};
