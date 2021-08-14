2d6ec0fafddb3cee17dd907558785f6d11a5e261
TwOnEnd <58563421+TwOnEnd@users.noreply.github.com>
2021-08-15 04:01:38 +0800

    Update 符号定位.hpp

--- ExplodeLog/BDS插件/符号定位.hpp
+++ ExplodeLog/BDS插件/符号定位.hpp
@@ -1,12 +1,24 @@
 #pragma once
-// [原型] public: virtual class std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> > const & __ptr64 __cdecl Actor::getNameTag(void)const __ptr64
-// [符号] ?getNameTag@Actor@@UEBAAEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@XZ
-constexpr RVA MSSYM_MD5_7044ab83168b0fd345329e6566fd47fd = 0x00BA2660;
+// [鍘熷瀷] public: virtual class std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> > const & __ptr64 __cdecl Actor::getNameTag(void)const __ptr64
+// [绗﹀彿] ?getNameTag@Actor@@UEBAAEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@XZ
+constexpr RVA MSSYM_MD5_7044ab83168b0fd345329e6566fd47fd = 0x00BA26C0;
 
-// [原型] protected: virtual bool __cdecl Mob::_hurt(class ActorDamageSource const & __ptr64,int,bool,bool) __ptr64
-// [符号] ?_hurt@Mob@@MEAA_NAEBVActorDamageSource@@H_N1@Z
-constexpr RVA MSSYM_B2QUA4hurtB1AA3MobB2AAA4MEAAB1UE22NAEBVActorDamageSourceB2AAA1HB1UA2N1B1AA1Z = 0x00CCAA20;
+// [鍘熷瀷] protected: virtual bool __cdecl Mob::_hurt(class ActorDamageSource const & __ptr64,int,bool,bool) __ptr64
+// [绗﹀彿] ?_hurt@Mob@@MEAA_NAEBVActorDamageSource@@H_N1@Z
+constexpr RVA MSSYM_B2QUA4hurtB1AA3MobB2AAA4MEAAB1UE22NAEBVActorDamageSourceB2AAA1HB1UA2N1B1AA1Z = 0x00CCAAB0;
 
-// [原型] public: virtual void __cdecl Level::explode(class BlockSource & __ptr64,class Actor * __ptr64,class Vec3 const & __ptr64,float,bool,bool,float,bool) __ptr64
-// [符号] ?explode@Level@@UEAAXAEAVBlockSource@@PEAVActor@@AEBVVec3@@M_N3M3@Z
-constexpr RVA MSSYM_B1QA7explodeB1AA5LevelB2AAE20UEAAXAEAVBlockSourceB2AAA9PEAVActorB2AAA8AEBVVec3B2AAA1MB1UA4N3M3B1AA1Z = 0x00F854C0;
+// [鍘熷瀷] public: virtual void __cdecl Level::explode(class BlockSource & __ptr64,class Actor * __ptr64,class Vec3 const & __ptr64,float,bool,bool,float,bool) __ptr64
+// [绗﹀彿] ?explode@Level@@UEAAXAEAVBlockSource@@PEAVActor@@AEBVVec3@@M_N3M3@Z
+constexpr RVA MSSYM_B1QA7explodeB1AA5LevelB2AAE20UEAAXAEAVBlockSourceB2AAA9PEAVActorB2AAA8AEBVVec3B2AAA1MB1UA4N3M3B1AA1Z = 0x00F85630;
+
+// [鍘熷瀷] public: virtual void __cdecl Level::forEachPlayer(class std::function<bool __cdecl(class Player const & __ptr64)>)const __ptr64
+// [绗﹀彿] ?forEachPlayer@Level@@UEBAXV?$function@$$A6A_NAEBVPlayer@@@Z@std@@@Z
+constexpr RVA MSSYM_B1QE13forEachPlayerB1AA5LevelB2AAA6UEBAXVB2QDA8functionB3ADDA3A6AB1UE11NAEBVPlayerB3AAAA1ZB1AA3stdB3AAAA1Z = 0x00F89760;
+
+// [鍘熷瀷] public: class Level * __ptr64 __cdecl Minecraft::getLevel(void)const __ptr64
+// [绗﹀彿] ?getLevel@Minecraft@@QEBAPEAVLevel@@XZ
+constexpr RVA MSSYM_B1QA8getLevelB1AA9MinecraftB2AAE13QEBAPEAVLevelB2AAA2XZ = 0x00BDA570;
+
+// [鍘熷瀷] public: virtual void __cdecl ServerScoreboard::onPlayerJoined(class Player const & __ptr64) __ptr64
+// [绗﹀彿] ?onPlayerJoined@ServerScoreboard@@UEAAXAEBVPlayer@@@Z
+constexpr RVA MSSYM_B1QE14onPlayerJoinedB1AE16ServerScoreboardB2AAE15UEAAXAEBVPlayerB3AAAA1Z = 0x0123B7E0;
